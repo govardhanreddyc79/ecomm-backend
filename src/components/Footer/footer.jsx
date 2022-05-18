a846f739a67d3fbcd416021254eaa74ea1fd45d6
govardhan-aviso <govardhanreddyc79@gmail.com>
2022-05-18 14:12:48 +0530

    added rel in footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -8,12 +8,12 @@ export const Footer = () =>{
                 <div class="footer-header">Social media presence
                     <ul class="social-links">
                         <li class="list-item-inline">
-                            <a class="link" href="https://github.com/govardhanreddyc79" target="_blank">Github</a>
+                            <a class="link" href="https://github.com/govardhanreddyc79" target="_blank" rel="noreferrer">Github</a>
                             </li>
                         <li class="list-item-inline">
-                            <a class="link" href="https://twitter.com/reddygov16" target="_blank">Twitter</a></li>
+                            <a class="link" href="https://twitter.com/reddygov16" target="_blank" rel="noreferrer">Twitter</a></li>
                         <li class="list-item-inline">
-                            <a class="link" href="https://www.linkedin.com/in/govardhan-reddy-1bb838136" target="_blank">LinkedIn</a></li>
+                            <a class="link" href="https://www.linkedin.com/in/govardhan-reddy-1bb838136" target="_blank" rel="noreferrer">LinkedIn</a></li>
                     </ul>
         </div>
 
